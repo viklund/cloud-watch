101a9f21dc7e8f001455510ff9d62355a9142d1e
Johan Viklund <johan.viklund@nbis.se>
2019-05-24 16:41:53 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name             = 'cloud-watch',
-        version          = '0.0.1',
+        version          = '0.0.2',
 
         author           = 'Johan Viklund',
         author_email     = 'johan.viklund@gmail.com',
